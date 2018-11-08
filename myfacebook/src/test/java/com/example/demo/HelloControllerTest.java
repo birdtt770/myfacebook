f4585c365ba95fff23b9eab6b508e883508aaa98
bird <birdtt770@gmail.com>
2018-11-08 13:55:07 +0700

    edit file

--- myfacebook/src/test/java/com/example/demo/HelloControllerTest.java
+++ myfacebook/src/test/java/com/example/demo/HelloControllerTest.java
@@ -31,7 +31,7 @@ public class HelloControllerTest {
  
     @Test
     public void hello_withName_mustEqualsParameter() throws Exception {
-  String name = "birdtt7700000";
+  String name = "Danai kongseeya";
  
         mockMvc.perform(MockMvcRequestBuilders.get("/hello?name=" + name).accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
